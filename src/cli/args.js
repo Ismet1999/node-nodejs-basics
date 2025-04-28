f9dd6b0b290ffdc3851816de06e9dad26888ca30
ismet <kadyrovismet1999@gmail.com>
2025-04-29 00:55:59 +0500

    feat: add cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const parseArgs = {};
+
+  for (let i = 0; i < args.length; i += 2) {
+    const key = args[i].slice(2);
+    const value = args[i + 1];
+    parseArgs[key] = value;
+  }
+
+  const result = Object.entries(parseArgs)
+    .map(([key, value]) => `${key} is ${value}`)
+    .join(", ");
+
+  console.log(result);
 };
 
 parseArgs();

973a357455f62b77fcf4ea76b3fe344d30dc3e7e
ismet <kadyrovismet1999@gmail.com>
2025-04-29 02:14:03 +0500

    feat: added calculate hash func

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import { createHash } from "crypto";
+import { createReadStream } from "fs";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import { pipeline } from "stream/promises";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash("sha256");
+  const filePath = join(__dirname, "files", "fileToCalculateHashFor.txt");
+  const stream = createReadStream(filePath);
+
+  await pipeline(stream, hash);
+
+  const hashHex = hash.digest("hex");
+  console.log(hashHex);
 };
 
 await calculateHash();
